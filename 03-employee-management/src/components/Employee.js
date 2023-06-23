1357cf90045e391dead973d4c41c8d41179cfb6f
bengisu-sahin <bengisusahin99@gmail.com>
2023-06-23 17:53:44 +0300

    Types of Hooks, useContext, UUID V4

--- 03-employee-management/src/components/Employee.js
+++ 03-employee-management/src/components/Employee.js
@@ -1,10 +1,27 @@
-const Employee = () => {
+const Employee = ({ employees }) => {
+    /**
+            React bileşenleri genellikle bir kök (root) element içinde döndürülür. Yani, bir bileşenin dönüş değeri olarak sadece bir ana element olması beklenir. Ancak, bazen birden fazla JSX elementini birleştirmek veya birleştirilmiş elementleri döndürmek gerekebilir.
+            Bu durumda, React fragmentleri (<> ve </>) kullanılabilir. Fragmentler, bir kök elemente ihtiyaç duymadan birden fazla elementi gruplamak için kullanılır. Fragmentler, görüntülenmeyen bir wrapper elementi oluşturmadan elementleri birleştirmek için ideal bir yöntemdir.
+             */
     return (
-        <ul>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-        </ul>
+
+        <>
+            {
+                employees.map((employee) => (
+                    <tr key={employee.id}>
+                        <td>{employee.name}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.address}</td>
+                        <td>{employee.phone}</td>
+                        <td>
+                            <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                            <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                        </td>
+                    </tr>
+                ))
+            }
+        </>
+
     )
 }
 
