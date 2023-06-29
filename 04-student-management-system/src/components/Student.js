9109a19509a1d47641b23f2b4071f9be2ad6cbb6
bengisu-sahin <bengisusahin99@gmail.com>
2023-06-29 14:26:33 +0300

    Students Table is done.

--- 04-student-management-system/src/components/Student.js
+++ 04-student-management-system/src/components/Student.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createContext, useEffect, useReducer, useState } from "react";
-import {StudentContext} from "../contexts/StudentContext";
+import { StudentContext } from "../contexts/StudentContext";
+import { Button, Modal, OverlayTrigger, Tooltip, Table } from 'react-bootstrap';
 
-const Student = () => {
+const Student = ({student}) => {
+    const departments = {
+        "1": "Bilgisayar Müh.",
+        "2": "Elektrik-Elektronik Müh.",
+        "3": "Endüstri Müh.",
+        "4": "İnşaat Müh."
+    };
     return (
-        <div className='container'>
-            <div className='row'>
-                <h1>HELLO</h1>
-            </div>
-        </div>
+        <>
+            <td className='col-3'>{student.fname.concat(" ", student.lname)}</td>
+            <td className='col-3'>{student.num}</td>
+            <td className='col-3'>{departments[student.dept]}</td>
+            <td className='col-3'>
+                <Button variant="danger" className="btn-primary col-3">Sil</Button>{' '}
+                <Button variant="primary" className="btn-primary col-3">Düzenle</Button>{' '}
+                <Button variant="success" className="btn-primary col-3">Detay</Button>{' '}
+            </td>
+        </>
     );
 }
 
--- 04-student-management-system/src/components/StudentList.js
+++ 04-student-management-system/src/components/StudentList.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import { useContext } from 'react';
 import { StudentContext } from '../contexts/StudentContext';
-import { Button, Modal, OverlayTrigger, Tooltip,Table } from 'react-bootstrap';
+import { Button, Modal, OverlayTrigger, Tooltip, Table } from 'react-bootstrap';
 import Student from "./Student";
 
 const StudentList = () => {
     const { students } = useContext(StudentContext);
+
     return (
-        <div className='student-list-container'>
-            <div className='row'>
-                <div className='col-6'> <h3>Öğrenci Listesi</h3></div>
-                <div className='col-6'><Button variant="primary" className="btn-primary">Add</Button></div>
-            </div>
-            
-                <Table className='table-striped x'>
-                    <thead>
-                        <tr>
-                            <th>İsim Soyisim</th>
-                            <th>Öğrenci Numarası</th>
-                            <th>Bölüm</th>
-                            <th>Yetkiler</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {students.map((student) => (
-                            <tr key={student.id}>
-                                <td>{student.fname.concat(" ", student.lname)}</td>
-                                <td>{student.num}</td>
-                                <td>Bilgisayar Mühendisliği</td> 
-                                <td>
-                                    <Button variant="danger" className="btn-primary">Sil</Button>{' '}
-                                    <Button variant="primary" className="btn-primary">Düzenle</Button>{' '}
-                                    <Button variant="success" className="btn-primary">Detay</Button>{' '}
-                                </td>
+        <>
+            <div className='student-list-container container'>
+                <div className='row'>
+                    <div className='col-11'>
+                        <h3>Öğrenci Listesi</h3>
+                    </div>
+                    <div className='col-1'>
+                        <Button variant="primary" className="btn-primary">Add</Button>
+                    </div>
+                </div>
+
+                <div className='row'>                   
+                    <table className='table-students table'>
+                        <thead className='table-light'>
+                            <tr>
+                                <th className="col-3">İsim Soyisim</th>
+                                <th className="col-3">Öğrenci Numarası</th>
+                                <th className="col-3">Bölüm</th>
+                                <th className="col-3">Yetkiler</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
+                        </thead>
+                        <tbody>
+                            {students.map((student) => (
+                                <tr key={student.id}>
+                                    <Student student={student} />
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-     
+        </>
     );
 };
-
 
 export default StudentList;
