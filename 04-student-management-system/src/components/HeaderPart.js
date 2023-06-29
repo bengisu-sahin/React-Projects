edbb80dd39aca48136703205bcf55f69a937d770
bengisu-sahin <bengisusahin99@gmail.com>
2023-06-29 15:43:46 +0300

    Header Part is done.

--- 04-student-management-system/src/components/HeaderPart.js
+++ 04-student-management-system/src/components/HeaderPart.js
@@ -1,17 +1,35 @@
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import { createContext, useEffect, useReducer, useState } from "react";
+import { Button, Modal, OverlayTrigger, Tooltip, Table } from 'react-bootstrap';
+import logo from '../logo/esogu_logo.png'
 
 const HeaderPart = () => {
-
     return (
+        <>
         <div className='container'>
-            <div className='row'>
-                <h1>HELLO</h1>
+        <div className='row header-item'>
+                <div className='col-12'> <h5>Merhaba, John Doe</h5></div>
+                <div className='col-12'> <Button variant='secondary' >Çıkış Yap</Button></div>
             </div>
-        </div>
+            <div className='row header-item'>
+                <div className='logo-container col-6'>
+                    <div className='row'>
+                        <div className='col-3'>
+                        <img src={logo} alt="Logo" className='esogu-logo ' />
+                        </div>
+                        <div className='col-3'>
+                        <span className='logo-txt'>Eskişehir Osmangazi Üniversitesi</span>
+                        <br></br> 
+                        <span className='logo-txt-2'>Öğrenci Yönetim Sistemi</span>
+                        </div>
+                    </div>       
+                </div>
+            </div>
+        </div> 
+        <br></br> 
+        </>
     );
 };
-
 
 export default HeaderPart;
