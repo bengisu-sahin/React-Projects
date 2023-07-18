bfaf9cc39aa3a76e2aa2c0d363c15afa996c8ab0
bengisu-sahin <bengisusahin99@gmail.com>
2023-07-18 16:01:11 +0300

    Homepage & Rouitin System & Nested Routes

---
+++ 06-next.js-crash-course/next.js-crash-course/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>AboutPage</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure tenetur
+                ducimus laboriosam eaque recusandae tempora enim aut, quae error eum
+                ipsum reiciendis temporibus dolore delectus ullam impedit cumque id.
+                Itaque?
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
