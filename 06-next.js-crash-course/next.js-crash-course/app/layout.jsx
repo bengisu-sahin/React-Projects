0b866af653319bc491247c61ec3c4b165a9a3326
bengisu-sahin <bengisusahin99@gmail.com>
2023-07-19 13:45:35 +0300

    Data Fetching & Custom Loading Page

--- 06-next.js-crash-course/next.js-crash-course/app/layout.jsx
+++ 06-next.js-crash-course/next.js-crash-course/app/layout.jsx
@@ -1,12 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Poppins } from 'next/font/google'
+import Header from './components/Header';
 
 const poppins = Poppins({
   weight: ['400', "700"],
   subsets: ['latin'],
 });
-//const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Next.js-13 Course',
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Header />
         <main className='container'>
           {children}
         </main>
