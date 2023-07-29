9428ae9bb27a077434e56c3b8b4defd9a26e27ff
bengisu-sahin <bengisusahin99@gmail.com>
2023-07-29 19:46:53 +0300

    Project Initialization

---
+++ 07-weather-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App" >
+      hello
+    </div>
+  );
+
+
+};
+
+export default App;
