ed89da1e5c7e0971bff07fbbe48c273072bb0355
Bengisu <71591780+bengisu-sahin@users.noreply.github.com>
2023-07-30 17:01:24 +0300

    Update api.js

--- 07-weather-app/src/api.js
+++ 07-weather-app/src/api.js
@@ -2,10 +2,10 @@ export const GEO_API_URL ='https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'db490e27b33mshb5ad9b38c221781p1e84fdjsn39f9d56fa98sf',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const WEATHER_API_URL ='https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = "5b2f85b1214793b83bc980ac64c88873sc"; 
+export const WEATHER_API_KEY = ""; 
